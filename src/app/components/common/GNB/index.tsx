a5e37589bd1629f5ca2a9a1ae0573bbd6c5da25b
jinha <jinha6707@gmail.com>
2024-03-25 12:43:44 +0500

    🚧 feature: GNB Component.

---
+++ src/app/components/common/GNB/index.tsx
@@ -0,0 +1,23 @@
+import Icon from "../Icon";
+import Text from "../Text";
+
+const GNB = () => {
+  return (
+    <footer className="w-[390px] h-16 flex justify-between px-20">
+      <div className="flex flex-col items-center">
+        <Icon type="recommend" size="medium" alt="추천받기" />
+        <Text size="extraSmall" className="text-black">
+          추천받기
+        </Text>
+      </div>
+      <div className="flex flex-col items-center">
+        <Icon type="account" size="medium" alt="로그인" />
+        <Text size="extraSmall" className="text-black">
+          로그인
+        </Text>
+      </div>
+    </footer>
+  );
+};
+
+export default GNB;
