08e7dc0949cc85d73eaa569980066c7026ae504f
jinha <jinha6707@gmail.com>
2024-03-25 10:31:32 +0500

    🚧 feature: Button Component.

---
+++ src/app/components/common/Button/Button.stories.tsx
@@ -0,0 +1,45 @@
+import type { Meta, StoryObj } from "@storybook/react";
+
+import Button from ".";
+
+import Icon from "../Icon";
+import Text from "../Text";
+
+const meta: Meta<typeof Button> = {
+  title: "Common/Button",
+  component: Button,
+  tags: ["autodocs"],
+};
+
+export default meta;
+
+type Story = StoryObj<typeof Button>;
+
+export const TextButton: Story = {
+  args: {
+    size: "full",
+    children: "버튼",
+    onClick: () => {
+      console.log("clicked");
+    },
+  },
+};
+
+export const IconButton: Story = {
+  args: {
+    size: "medium",
+    children: (
+      <>
+        <Icon type="edit" alt="리뷰쓰기" />
+        <Text size="medium" className="text-black">
+          리뷰 쓰기
+        </Text>
+      </>
+    ),
+    onClick: () => {
+      console.log("clicked");
+    },
+    className:
+      "bg-white border-[1px] border-solid border-gray-400 enabled:hover:bg-primary-100",
+  },
+};
