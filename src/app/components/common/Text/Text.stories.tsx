02c0ef08753e4abae60ae5e5bc8c9cf20e140633
jinha <jinha6707@gmail.com>
2024-03-25 08:14:38 +0500

    🚧 feature Icon Component.

---
+++ src/app/components/common/Text/Text.stories.tsx
@@ -0,0 +1,21 @@
+import Text from ".";
+import type { Meta, StoryObj } from "@storybook/react";
+
+const meta: Meta<typeof Text> = {
+  title: "atoms/Text",
+  component: Text,
+  tags: ["autodocs"],
+};
+
+export default meta;
+
+type Story = StoryObj<typeof Text>;
+
+export const Default: Story = {
+  args: {
+    size: "large",
+    weight: "bold",
+    children: "텍스트",
+    className: "text-black",
+  },
+};
