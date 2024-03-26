709f78f24a8f6b873cbfabb4ab053ba7513c40ba
jinha <jinha6707@gmail.com>
2024-03-26 21:28:34 +0500

    🚧 feature: Filter - Condition, Tag feat.

--- src/components/Condition/index.tsx
+++ src/components/Condition/index.tsx
@@ -1,8 +1,23 @@
 import { filterDatas } from "@/app/constants";
 import Text from "../common/Text";
 import Tag from "../common/Tag";
+import { useState } from "react";
+
+type SelectedItemsState = {
+  [filterId: string]: string | undefined;
+};
 
 const Condition = () => {
+  const [selectedItems, setSelectedItems] = useState<SelectedItemsState>({});
+
+  const handleItemClick = (filterId: string, itemId: string) => {
+    setSelectedItems((prevState) => ({
+      ...prevState,
+      [filterId]: itemId,
+    }));
+  };
+
+  console.log(selectedItems);
   return (
     <div className="px-4 py-6 border-t-[1px] border-b-[1px] border-silver">
       {filterDatas.map((condition) => {
@@ -13,7 +28,14 @@ const Condition = () => {
             </Text>
             <div className="flex gap-2 flex-wrap mb-[30px]">
               {condition.data.map((d) => {
-                return <Tag key={d.id} tagName={d.name} />;
+                return (
+                  <Tag
+                    key={d.id}
+                    tagName={d.name}
+                    isSelected={selectedItems[condition.id] === d.id}
+                    handleItemClick={() => handleItemClick(condition.id, d.id)}
+                  />
+                );
               })}
             </div>
           </div>
