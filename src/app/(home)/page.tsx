74ce659e790da1e7ea06016b1c6395e440d6e755
jinha <jinha6707@gmail.com>
2024-03-26 20:45:46 +0500

    ♻️ refactor: root component add Suspeonse

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -2,15 +2,18 @@
 
 import HomeSection from "@/components/Home/HomeSection";
 import KakaoMap from "@/components/KakaoMap";
+import { Suspense } from "react";
 
 export default function Home() {
   return (
     <main className="relative">
-      <KakaoMap>
-        <div className="w-full absolute top-8 z-[999]">
-          <HomeSection />
-        </div>
-      </KakaoMap>
+      <Suspense fallback={<div>Loading...</div>}>
+        <KakaoMap>
+          <div className="w-full absolute top-8 z-[999]">
+            <HomeSection />
+          </div>
+        </KakaoMap>
+      </Suspense>
     </main>
   );
 }
--- src/app/search_map/page.tsx
+++ src/app/search_map/page.tsx
@@ -2,16 +2,19 @@
 
 import KakaoMap from "@/components/KakaoMap";
 import HomeSection from "@/components/Home/HomeSection";
+import { Suspense } from "react";
 
 export default function SearchMapPage() {
   return (
     <main className="relative">
       <section>
-        <KakaoMap>
-          <div className="w-full absolute top-8 z-[999]">
-            <HomeSection />
-          </div>
-        </KakaoMap>
+        <Suspense fallback={<div>Loading...</div>}>
+          <KakaoMap>
+            <div className="w-full absolute top-8 z-[999]">
+              <HomeSection />
+            </div>
+          </KakaoMap>
+        </Suspense>
       </section>
     </main>
   );
