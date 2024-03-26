518e36b257162d0470ec1432735271fd98d5c2d6
jinha <jinha6707@gmail.com>
2024-03-26 09:43:09 +0500

    🚧 feature: Page Component ing... / search, search_map page .

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,12 +1,20 @@
+"use client";
+
+import TopSection from "@/components/Home/TopSection";
 import KakaoMap from "@/components/KakaoMap";
+
 import GNB from "@/components/common/GNB";
+import { defaultCoords } from "../constants";
 
 export default function Home() {
   return (
-    <main>
-      <KakaoMap />
-      <div></div>
-      <GNB />
+    <main className="relative">
+      <KakaoMap>
+        <div className="w-full px-4 absolute top-8 z-[999]">
+          <TopSection />
+        </div>
+        <GNB />
+      </KakaoMap>
     </main>
   );
 }
