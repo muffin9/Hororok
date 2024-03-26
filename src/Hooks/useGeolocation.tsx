1f78ec554b3560f3bd97d7e5ea2f78b9fb854311
jinha <jinha6707@gmail.com>
2024-03-26 19:54:57 +0500

    🚧 feature: HomeSection, KakaoMap & location feat.

---
+++ src/Hooks/useGeolocation.tsx
@@ -0,0 +1,31 @@
+import { defaultCoords } from "@/app/constants";
+import { useEffect, useState } from "react";
+
+function useGeolocation() {
+  const [location, setLocation] = useState({
+    latitude: defaultCoords.lat,
+    longitude: defaultCoords.lng,
+  });
+
+  useEffect(() => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          setLocation({
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude,
+          });
+        },
+        (error) => {
+          console.error("Error getting geolocation:", error);
+        }
+      );
+    } else {
+      console.log("Geolocation is not supported by this browser.");
+    }
+  }, []);
+
+  return location;
+}
+
+export default useGeolocation;
