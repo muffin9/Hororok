464b408673de806e0a49b6aeac6c89cc0c57e73b
jinha <jinha6707@gmail.com>
2024-03-27 11:40:04 +0500

    ♻️ refactor: HomeSection Component separation / Filter Component naming add section.

---
+++ src/components/FilterSection/FilterSection.stories.tsx
@@ -0,0 +1,21 @@
+import type { Meta } from "@storybook/react";
+import FilterSection from ".";
+
+const meta: Meta<typeof FilterSection> = {
+  title: "Section/FilterSection",
+  component: FilterSection,
+};
+
+export default meta;
+
+const PrimitiveFilterSection = () => {
+  return (
+    <FilterSection
+      onCloseButton={() => console.log("closed FilterSection section")}
+    />
+  );
+};
+
+export const Default = {
+  render: () => <PrimitiveFilterSection />,
+};
--- src/components/Home/HomeSection/index.tsx
+++ src/components/Home/HomeSection/index.tsx
@@ -1,50 +1,37 @@
-import Filter from "@/components/Filter";
+import FilterSection from "@/components/FilterSection";
 
 import GNB from "@/components/common/GNB";
-import Icon from "@/components/common/Icon";
 import Input from "@/components/common/Input";
-import Category from "@/components/common/Category";
 import { useRouter } from "next/navigation";
 import { useState } from "react";
+import CategoryList from "@/components/common/CategoryList/CategoryList";
 
-const HomeSection = () => {
+interface HomeSectionProps {
+  isInput?: boolean;
+}
+
+const HomeSection = ({ isInput }: HomeSectionProps) => {
   const router = useRouter();
   const [isFilter, setIsFilter] = useState(false);
 
-  const categoryNames = [
-    { id: 1, name: "목적" },
-    { id: 2, name: "시설" },
-    { id: 3, name: "분위기" },
-    { id: 4, name: "메뉴" },
-    { id: 5, name: "테마" },
-  ];
-
   return (
     <section className="relative">
-      <div className="px-4">
-        <Input
-          type="text"
-          placeholder="어떤 카페가 궁금하세요?"
-          onClick={() => router.push("/search")}
-          className="w-full h-[50px] p-4 bg-white border-[1xps] border-gray-400 rounded-lg focus:outline-none"
-        />
-      </div>
+      {isInput && (
+        <div className="px-4">
+          <Input
+            type="text"
+            placeholder="어디 근처 카페 찾으세요?"
+            onClick={() => router.push("/search")}
+            className="w-full h-[50px] p-4 bg-white border-[1xps] border-gray-400 rounded-lg focus:outline-none"
+          />
+        </div>
+      )}
       {isFilter ? (
-        <Filter onCloseButton={() => setIsFilter(false)} />
+        <FilterSection onCloseButton={() => setIsFilter(false)} />
       ) : (
         <>
-          <div className="w-full flex justify-between mt-4 px-4">
-            {categoryNames.map((tag) => {
-              return (
-                <Category
-                  key={tag.id}
-                  name={tag.name}
-                  icon={<Icon type="arrow_under" size="xSmall" alt="더보기" />}
-                  posIcon="right"
-                  onClick={() => setIsFilter(true)}
-                />
-              );
-            })}
+          <div className="px-4 mt-4">
+            <CategoryList onFilter={() => setIsFilter(true)} />
           </div>
           <GNB />
         </>
