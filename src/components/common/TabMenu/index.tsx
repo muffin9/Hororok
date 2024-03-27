8a156a75e3dc84ac88994e6a0b574a23bc2cc6a2
jinha <jinha6707@gmail.com>
2024-03-27 19:46:40 +0500

    🚧 feature: TabMenu Component

---
+++ src/components/common/TabMenu/index.tsx
@@ -0,0 +1,24 @@
+interface TabMenuProps {
+  menuInfo: { id: string; name: string; isClicked: boolean }[];
+  handleOnMenuClick: (clickedId: string) => void;
+}
+
+const TabMenu = ({ menuInfo, handleOnMenuClick }: TabMenuProps) => {
+  return (
+    <nav className="flex justify-between px-4 pt-6 text-black border-b-[1px] border-solid border-silver">
+      {menuInfo.map((menu) => {
+        return (
+          <button
+            key={menu.id}
+            className={`w-[90px] pb-4 font-bold text-gray-700 ${menu.isClicked && "border-b-[2px]"} border-solid border-black`}
+            onClick={() => handleOnMenuClick(menu.id)}
+          >
+            {menu.name}
+          </button>
+        );
+      })}
+    </nav>
+  );
+};
+
+export default TabMenu;
