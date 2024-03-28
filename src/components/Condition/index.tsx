2329f60e4caeca6b439a0532312806f9f6ce024d
jinha <jinha6707@gmail.com>
2024-03-28 19:42:02 +0500

    ♻️ Condition Component FilterData calculated

--- src/components/Condition/index.tsx
+++ src/components/Condition/index.tsx
@@ -1,16 +1,35 @@
 import { filterDatas } from "@/app/constants";
 import Text from "../common/Text";
 import Tag from "../common/Tag";
+import { CategoryIdType } from "../Home/HomeSection";
 
 interface ConditionProps {
+  categoryId?: CategoryIdType;
   handleItemClick: (filterId: string, itemId: string) => void;
   checkSelected: (conditionId: string, clickedItemId: string) => boolean;
 }
 
-const Condition = ({ handleItemClick, checkSelected }: ConditionProps) => {
+const Condition = ({
+  categoryId,
+  handleItemClick,
+  checkSelected,
+}: ConditionProps) => {
+  const calculatedFilterData = () => {
+    if (categoryId) {
+      const clickedCategoryIdx = filterDatas.findIndex(
+        (data) => data.id === categoryId
+      );
+      if (clickedCategoryIdx !== -1) {
+        const clickedCategory = filterDatas.splice(clickedCategoryIdx, 1)[0];
+        filterDatas.unshift(clickedCategory);
+      }
+      return filterDatas;
+    } else return filterDatas;
+  };
+
   return (
     <div className="py-6 border-t-[1px] border-b-[1px] border-silver">
-      {filterDatas.map((condition) => {
+      {calculatedFilterData().map((condition) => {
         return (
           <div key={condition.id} className="flex flex-col gap-4">
             <header className="flex items-center gap-2">
