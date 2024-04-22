a6ca898a2b183dde9bcf38b3ac66a49dde135a9c
jinha <jinha6707@gmail.com>
2024-04-22 14:46:52 +0900

    ♻️ refactor: useToggleBookmark func refac.

--- src/Hooks/Api/useToggleBookmark.tsx
+++ src/Hooks/Api/useToggleBookmark.tsx
@@ -8,14 +8,18 @@ const useToggleBookmark = () => {
   const toggleBookmark = async (cafeId: number, folderId: number) => {
     // TODO : 에러 수정 필요.
     const { data: folderList } = await getFolderList(folderId);
-    const findFolder: FolderType = folderList.bookmarks.find(
-      (folder: FolderType) => {
-        return folder.folderId === folderId;
-      }
-    );
+    if (folderList.bookmarks.length > 0) {
+      const findFolder: FolderType = folderList.bookmarks.find(
+        (folder: FolderType) => {
+          return folder.folderId === folderId;
+        }
+      );
 
-    if (findFolder) {
-      deleteBookmark(findFolder.folderId);
+      if (findFolder) {
+        deleteBookmark(findFolder.folderId);
+      } else {
+        postBookmark({ cafeId, folderId });
+      }
     } else {
       postBookmark({ cafeId, folderId });
     }
