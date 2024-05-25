96621289b1d9f706ab68cac0430ff75ee5b36b85
jinha <jinha6707@gmail.com>
2024-05-25 18:18:35 +0900

    ♻️ refactor: Home Page if accessToken? router push map apply.

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -1,38 +1,27 @@
 "use client";
 
-import { useEffect, Suspense } from "react";
+import { Suspense } from "react";
 import Splash from "../common/Splash";
 import Loading from "@/app/loading";
-import useSplashState from "@/store/useSplash";
 import LoginComponent from "../common/LoginComponent";
 import useIsLoggedIn from "@/Hooks/useLoggedIn";
 import { useRouter } from "next/navigation";
 
 const Home = () => {
   const router = useRouter();
-  const { showSplash, setShowSplash } = useSplashState();
+
   const isLoggedIn = useIsLoggedIn();
 
-  useEffect(() => {
-    setShowSplash(true);
-
-    if (isLoggedIn) router.push("/map");
-  }, []);
+  if (isLoggedIn) router.push("/map");
 
   return (
     <>
-      {!showSplash ? (
+      <Suspense fallback={<Loading />}>
         <main className="w-full h-full px-4 flex flex-col items-center justify-center bg-primary-300">
           <Splash />
+          <LoginComponent />
         </main>
-      ) : (
-        <Suspense fallback={<Loading />}>
-          <main className="w-full h-full px-4 flex flex-col items-center justify-center bg-primary-300">
-            <Splash />
-            <LoginComponent />
-          </main>
-        </Suspense>
-      )}
+      </Suspense>
     </>
   );
 };
