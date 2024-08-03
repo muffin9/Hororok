2bc6a973d9648cb0535483caa6d29670f29eef28
jinha <jinha6707@gmail.com>
2024-08-03 20:22:39 +0900

    ♻️ refac: mainPage Componen add instagram, OnBoarding image update apply.

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -7,7 +7,9 @@ export default function Login() {
   return (
     <main className="w-full h-full px-4 flex flex-col items-center justify-center bg-primary-300">
       <Splash />
-      <LoginComponent />
+      <div className="fixed bottom-16">
+        <LoginComponent />
+      </div>
     </main>
   );
 }
--- src/components/OnBoarding/index.tsx
+++ src/components/OnBoarding/index.tsx
@@ -5,7 +5,6 @@ import Indicator from "../common/Indicator";
 import Text from "../common/Text";
 import { onBoardingTexts } from "@/app/constants";
 import Image from "next/image";
-import Button from "../common/Button";
 import { useRouter } from "next/navigation";
 import LoginComponent from "../common/LoginComponent";
 import Icon from "../common/Icon";
@@ -29,35 +28,39 @@ const OnBoarding = () => {
   };
 
   return (
-    <section className="h-full px-4 flex flex-col justify-center items-center gap-6 bg-white">
-      <Indicator count={onBoardingTexts.length} currentIdx={onStep} />
-      <Text size="x2Large">{onBoardingTexts[onStep].title}</Text>
-      <div className="flex flex-col">
-        {onBoardingTexts[onStep].description.map((descriptionText: string) => {
-          return (
-            <Text
-              key={descriptionText}
-              size="medium"
-              className="text-center text-gray-800"
-            >
-              {descriptionText}
-            </Text>
-          );
-        })}
-      </div>
-      <div className="flex gap-2">
-        <button onClick={handlePreviousStep}>
-          <Icon size="xSmall" type="arrow_left" alt="arrow_left" />
-        </button>
-        <Image
-          width={298}
-          height={480}
-          src={onBoardingTexts[onStep].imageUrl}
-          alt="onBoarding image"
-        />
-        <button onClick={handleNextStep}>
-          <Icon size="xSmall" type="arrow_right" alt="arrow_right" />
-        </button>
+    <section className="h-full px-4 flex flex-col justify-around items-center gap-6 bg-white">
+      <div className="flex flex-col items-center gap-4">
+        <Indicator count={onBoardingTexts.length} currentIdx={onStep} />
+        <Text size="x2Large">{onBoardingTexts[onStep].title}</Text>
+        <div className="flex flex-col">
+          {onBoardingTexts[onStep].description.map(
+            (descriptionText: string) => {
+              return (
+                <Text
+                  key={descriptionText}
+                  size="medium"
+                  className="text-center text-gray-800"
+                >
+                  {descriptionText}
+                </Text>
+              );
+            }
+          )}
+        </div>
+        <div className="flex gap-2">
+          <button onClick={handlePreviousStep}>
+            <Icon size="xSmall" type="arrow_left" alt="arrow_left" />
+          </button>
+          <Image
+            width={200}
+            height={397}
+            src={onBoardingTexts[onStep].imageUrl}
+            alt="onBoarding image"
+          />
+          <button onClick={handleNextStep}>
+            <Icon size="xSmall" type="arrow_right" alt="arrow_right" />
+          </button>
+        </div>
       </div>
       <LoginComponent />
     </section>
