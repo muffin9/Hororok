02b92266738d943b8238d672ccefd69018e48cd4
jinha <jinha6707@gmail.com>
2024-08-29 20:12:47 +0900

    ♻️ refac: css modify apply

--- src/components/PlanSection/NextButtonSection/index.tsx
+++ src/components/PlanSection/NextButtonSection/index.tsx
@@ -16,7 +16,7 @@ const NextButtonSection = ({
   const router = useRouter();
 
   return (
-    <div className="w-[358px] fixed bottom-4 flex flex-col gap-4 bg-white">
+    <div className="w-[358px] fixed bottom-0 flex flex-col gap-4 bg-white">
       {/* {nextStep === "2" && (
         <div
           className="flex flex-col items-center cursor-pointer"
